600e3be65386c40dd55083ac24256a46d26c29b2
pizzahunter239 <pizzahunter239@gmail.com>
2025-09-02 15:10:49 -0400

    MVCFirstApp

---
+++ MVCFirstApp/MVCFirstApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System.Diagnostics;
+
+namespace MVCFirstApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
